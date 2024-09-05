03794c1f28174444e60fba7ad96f493c52970f9c
Gregory Minho <gregoryminho@gmail.com>
2024-09-05 19:43:16 -0300

    troca de nome do arquivo

---
+++ Q1/questao1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio 1</title>
+</head>
+
+<body>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $age = $_POST["age"];
+
+
+        if (filter_var($age, FILTER_VALIDATE_INT) && $age > 0) {
+            if ($age >= 18) {
+                echo "Você pode se cadastrar!";
+            } else {
+                echo "Você não pode se cadastrar. Você deve ter pelo menos 18 anos.";
+            }
+        } else {
+            echo "Idade inválida. Por favor, insira uma idade válida.";
+        }
+    }
+    ?>
+
+    /*php retroalimentado abaixo:*/
+
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+        <label for="age">Idade:</label>
+        <input type="number" id="age" name="age" required>
+        <button type="submit">Verificar Idade</button>
+    </form>
+
+</body>
+
+</html>
