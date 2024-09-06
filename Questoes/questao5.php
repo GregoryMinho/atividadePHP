6bc1dbcd292aed766379ba56763fa4d19eb657b4
Gregory Minho <gregoryminho@gmail.com>
2024-09-05 21:18:18 -0300

    att

--- Questoes/questao5.php
+++ Questoes/questao5.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>exercicio 5</title>
 </head>
 
 <body>
     <?php
-
+    $choice = null;
     $balance = 0;
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
