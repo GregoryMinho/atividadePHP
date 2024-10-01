99fe67f10f945c152f45625985264bffe163fbf0
Gregory Minho <gregoryminho@gmail.com>
2024-10-01 19:13:08 -0300

    Create Questao 1.php

---
+++ Exercicio 200924/Questao 1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 20/09/24</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="nomePROD">Insira o NOME do produto:</label>
+        <input type="text" id="nomePROD" name="nomePROD" value="" required>
+        <label for="precoPROD">Insira o PREÇO do produto:</label>
+        <input type="number" name="precoPROD" id="precoPROD" value="" required>
+        
+    </form>
+</body>
+</html>
